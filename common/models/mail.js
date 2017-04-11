27cbde4df42c71356d34b3bfa9fb56eeedc3979e
milleraundra <miller.aundra@gmail.com>
2017-04-11 14:36:28 -0700

    create basic remote method inside mail.js

--- common/models/mail.js
+++ common/models/mail.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Mail) {
 
+  Mail.contact = function(sender, email, subject, body, cb) {
+    cb(null, "TADA!");
+  };
+
+  Mail.remoteMethod('contact', {
+    accepts: [
+      {arg: 'sender', type: 'string'},
+      {arg: 'email', type: 'string'},
+      {arg: 'subject', type: 'string'},
+      {arg: 'body', type: 'string'}
+    ],
+    returns: {arg: 'greeting', type: 'string'}
+
+  });
+
+
 };
